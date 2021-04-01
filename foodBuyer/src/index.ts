bac2541d617cc26ad07fd3aaa0bc2a66e17cb099
kylemoran138 <kylemoran.data@gmail.com>
2021-03-31 20:14:35 -0700

    AAAAA

--- foodBuyer/src/index.ts
+++ foodBuyer/src/index.ts
@@ -1,1 +1,14 @@
-console.log('sus')
+import puppeteer from 'puppeteer';
+
+const main = async () => {
+  const browser = await puppeteer.launch({
+    headless: false, 
+  });
+  
+  const page = await browser.newPage();
+  await page.setRequestInterception(true);
+  
+  await page.goto('https://www.google.com');
+}
+
+main();
