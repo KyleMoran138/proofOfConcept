fb51bb828f47b06a592abcfee7520cb449df59e2
kylemoran138 <kylemoran.data@gmail.com>
2021-05-26 17:27:52 -0700

    Ahhh, cached

--- foodBuyer/src/config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    "notion": {
-        "apiKey": "secret_85Hl5zHod56bd5WDBBNfIjU8MM5gP6X9PxI2fU3tSz6",
-        "databaseId": "bc63cd56-15c0-4fa0-bda2-9a87b539bbcc" //AKA database id
-    }
-}
