7d2e73f96a5ce8dc572b3a47c2cf34275f8128ff
kylemoran138 <kylemoran.data@gmail.com>
2021-06-14 15:43:36 -0700

    Revert "Fix some bugs"
    This reverts commit 7cd85414ca8162c4597f503bb1ea1ea37da637c8.

--- haasLightAutomation/src/State.ts
+++ haasLightAutomation/src/State.ts
@@ -18,7 +18,6 @@ class State {
     }
 
     this.data = {
-      ...previousData,
       timers: previousData?.timers || new Map<string, number[]>(),
       home: previousData?.home || {},
       event: inputs?.event || '',
@@ -118,7 +117,6 @@ class State {
   
           const timeoutId = setTimeout(() => {
             this.fireActions(timer.actions)
-            flow.set("stateData", this.data);
           }, timer.epochTimeToFire - new Date().getTime())
   
           timerIds.push(timeoutId);
